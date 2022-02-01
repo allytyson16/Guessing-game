f7725c26277ffe94aa8d2190d6be1ff8fb48648f
Adelaide <88384474+allytyson16@users.noreply.github.com>
2022-02-01 13:32:26 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,27 @@ namespace Udemyyyyy
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
-    }
+            Random randomGenerator = new Random();
+            int randomNumber = randomGenerator.Next(1, 11); 
+		
+		    Console.Write("Guess the random number - ");
+		    int guess = int.Parse(Console.ReadLine());
+		
+		    Console.WriteLine("The random number was {0}", randomNumber);
+		
+		    if(guess < randomNumber)
+		    {
+		    	Console.WriteLine("Your guess was too low.");
+		    }
+		    else if(guess > randomNumber)
+		    {
+		    	Console.WriteLine("Your guess was too high.");
+		    }
+		    else
+		    {
+		    	Console.WriteLine("Your guess was correct.");
+		    }
+			
+	  }
+   }
 }
